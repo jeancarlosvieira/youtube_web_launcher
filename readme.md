c67faea82e19579e515891c622416ba921520f7f
jcv <Jean Vieira>
2021-03-01 23:27:43 -0300

    Upload Script

---
+++ readme.md
@@ -0,0 +1,2 @@
+Simple youtube launcher that works on chromium.
+Just run the youtube_webapp.sh script and have fun. 
---
+++ youtube_webapp.sh
@@ -0,0 +1,1 @@
+chromium-browser ~/youtube_webapp/youtube_redirector.html --user-agent="Mozilla/5.0 (Linux; Tizen 2.3; SmartHub; SMART-TV; SmartTV; U; Maple2012) AppleWebKit/538.1+ (KHTML, like Gecko) TV Safari/538.1+" --start-fullscreen --kiosk --disable-restore-session-state --disable-features=TranslateUI --disable-session-crashed-bubble --disable-popup-blocking
